2ba647bc7ec0d910c750a8eae9800626e4aab8aa
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-08-09 18:17:57 +0200

    implement introduction of program and ask user about date of birdthday

--- DayGuesser.cs
+++ DayGuesser.cs
@@ -1,19 +1,31 @@
-﻿using WhatADayCore;
+﻿using System;
+using WhatADayCore;
 
 namespace WhatADayApp
 {
     public class DayGuesser
     {
         public DayCalculator Calculator { get; set; }
+        public DateTimeOffset UserDateOfBirth { get; set; }
 
         public void WelcomeText()
         {
-
+            Console.WriteLine("------ Hi, I am a program that calculates the day of the week you were born based on your date of birth!!! ------");
         }
 
         public void AskUserForDateOfBirth()
         {
+            Console.WriteLine("Enter your date of birth, please: ");
+            var userData = Console.ReadLine();
+            var succeded = DateTimeOffset.TryParse(userData, out var date);
 
+            if (succeded)
+            {
+                UserDateOfBirth = date;
+                return;
+            }
+            Console.WriteLine("The date format was wrong. Use dd/mm/yyyy please.");
+            AskUserForDateOfBirth();
         }
 
         public void CalculateDayOfTheWeek()
