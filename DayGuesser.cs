0008e34f06edf6885a9567a5ff062a32f19b93f9
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-08-09 17:57:16 +0200

    fix naming

--- DayGuesser.cs
+++ DayGuesser.cs
@@ -16,15 +16,11 @@ namespace WhatADayApp
 
         }
 
-<<<<<<< HEAD
         public void CalculateDayOfTheWeek()
         {
 
         }
         public void PrintDayOfTheWeek()
-=======
-        public void CalculateDateOfBirth()
->>>>>>> 25c57515a40efdd0c0999444711fbe636e5cd17d
         {
 
         }
