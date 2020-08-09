25c57515a40efdd0c0999444711fbe636e5cd17d
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-08-09 17:48:26 +0200

    make a structure of main app

--- DayGuesser.cs
+++ DayGuesser.cs
@@ -5,5 +5,20 @@ namespace WhatADayApp
     public class DayGuesser
     {
         public DayCalculator Calculator { get; set; }
+
+        public void WelcomeText()
+        {
+
+        }
+
+        public void AskUserForDateOfBirth()
+        {
+
+        }
+
+        public void CalculateDateOfBirth()
+        {
+
+        }
     }
 }
