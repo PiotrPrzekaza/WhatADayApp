a0e2ec2ae898409afd71e4aee0b413bc3dc95beb
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-08-11 17:44:57 +0200

    change a catalog of structure and add logic in guesser calculator

---
+++ WhatADayCore/DayCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhatADayCore
+{
+    public class DayCalculator
+    {
+        public DayOfWeek CalculateDayOfTheWeek(DateTimeOffset date)
+        {
+            var day = date.Day;
+            var month = date.Month;
+            var year = date.Year;
+
+            var daysList = new List<int> { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+            year -= month < 3 ? 1 : 0;
+
+            var calculateValue = (year + year / 4 - year / 100 + year / 400 + daysList[month - 1]+ day)%7;
+
+            return (DayOfWeek)(calculateValue - 1);
+        }
+    }
+}
