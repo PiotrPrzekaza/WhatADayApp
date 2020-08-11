8eea5a98e0e0b8f9181d4123afdaf5bf3d801bed
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-08-11 17:40:38 +0200

    added logic to guesser

--- DayGuesser.cs
+++ DayGuesser.cs
@@ -7,10 +7,12 @@ namespace WhatADayApp
     {
         public DayCalculator Calculator { get; set; }
         public DateTimeOffset UserDateOfBirth { get; set; }
+        public DayOfWeek UserDayOfWeek { get; set; }
 
         public void WelcomeText()
         {
             Console.WriteLine("------ Hi, I am a program that calculates the day of the week you were born based on your date of birth!!! ------");
+            Calculator = new DayCalculator();
         }
 
         public void AskUserForDateOfBirth()
@@ -24,17 +26,22 @@ namespace WhatADayApp
                 UserDateOfBirth = date;
                 return;
             }
-            Console.WriteLine("The date format was wrong. Use dd/mm/yyyy please.");
+            Console.WriteLine("The date format was wrong. Use mm/dd/yyyy please.");
             AskUserForDateOfBirth();
         }
 
         public void CalculateDayOfTheWeek()
         {
-
+            if(UserDateOfBirth  == null)
+            {
+                Console.WriteLine("I can't calculate day of birth without date. Please check you date of birth.");
+                return;
+            }
+             UserDayOfWeek =  Calculator.CalculateDayOfTheWeek(UserDateOfBirth);
         }
         public void PrintDayOfTheWeek()
         {
-
+            Console.WriteLine("A day of week, You are born is - " + UserDayOfWeek);
         }
     }
 }
